7cd2f11e38b4fd8dfd9483cb7960136225b8ab25
Kishore Nallan <kishorenc@gmail.com>
2022-04-26 16:46:17 +0530

    Add test for verifying retries.

---
+++ src/test/java/org/typesense/api/RetryTest.java
@@ -0,0 +1,38 @@
+package org.typesense.api;
+
+import junit.framework.TestCase;
+import org.typesense.resources.Node;
+
+import java.net.UnknownHostException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class RetryTest extends TestCase {
+
+    private Client client;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        ArrayList<Node> nodes = new ArrayList<>();
+        nodes.add(new Node("http","unknownhost123","8108"));
+        Configuration configuration = new Configuration(nodes, Duration.ofSeconds(1),"xyz");
+        configuration.numRetries = 2;
+        configuration.retryInterval = Duration.ofSeconds(2);
+        this.client = new Client(configuration);
+    }
+
+    public void testRetry() throws Exception {
+        long start = System.currentTimeMillis();
+        try {
+            client.health.retrieve();
+        } catch (Exception e) {
+            long timeTaken = System.currentTimeMillis() - start;
+            assertTrue(e.getCause() instanceof UnknownHostException);
+            assertTrue(timeTaken > 4000);
+            return ;
+        }
+
+        throw new Exception("Retry did not throw exception.");
+    }
+}
