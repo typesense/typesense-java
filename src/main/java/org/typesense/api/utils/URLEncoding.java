f2f544ef4819574f32268aab07945af215e4459f
Hayden <hayden.hhoang@gmail.com>
2024-09-07 21:57:39 +0700

    remove unnecessary URL encoding of special characters

--- src/main/java/org/typesense/api/utils/URLEncoding.java
+++ src/main/java/org/typesense/api/utils/URLEncoding.java
@@ -23,12 +23,7 @@ public class URLEncoding {
 
         try {
             result = URLEncoder.encode(s, "UTF-8")
-                    .replaceAll("\\+", "%20")
-                    .replaceAll("\\%21", "!")
-                    .replaceAll("\\%27", "'")
-                    .replaceAll("\\%28", "(")
-                    .replaceAll("\\%29", ")")
-                    .replaceAll("\\%7E", "~");
+                    .replaceAll("\\+", "%20");
         }
 
         // This exception should never occur.
