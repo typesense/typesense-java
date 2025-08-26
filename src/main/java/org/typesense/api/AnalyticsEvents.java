40e097720866b434a5567e497d514d7d183959bc
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-08-26 18:46:06 +0300

    feat(analytics): update analytic event class to new api

--- src/main/java/org/typesense/api/AnalyticsEvents.java
+++ src/main/java/org/typesense/api/AnalyticsEvents.java
@@ -1,8 +1,9 @@
 package org.typesense.api;
 
+import org.typesense.model.AnalyticsEvent;
 import org.typesense.model.AnalyticsEventCreateResponse;
-import org.typesense.model.AnalyticsEventCreateSchema;
-
+import org.typesense.model.AnalyticsEventsResponse;
+import java.util.Map;
 
 public class AnalyticsEvents {
     private final ApiCall apiCall;
@@ -12,7 +13,11 @@ public class AnalyticsEvents {
         this.apiCall = apiCall;
     }
 
-    public AnalyticsEventCreateResponse create(AnalyticsEventCreateSchema event) throws Exception {
+    public AnalyticsEventCreateResponse create(AnalyticsEvent event) throws Exception {
         return this.apiCall.post(RESOURCE_PATH, event, null, AnalyticsEventCreateResponse.class);
+    }
+
+    public AnalyticsEventsResponse retrieve(Map<String, Object> params) throws Exception {
+        return this.apiCall.get(RESOURCE_PATH, params, AnalyticsEventsResponse.class);
     }
 }
