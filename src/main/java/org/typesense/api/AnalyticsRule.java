9979908cad969eeaeb10193d943c814e4e5c6e30
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-08-26 18:46:13 +0300

    feat(analytics): update analytic rule classes to new api

--- src/main/java/org/typesense/api/AnalyticsRule.java
+++ src/main/java/org/typesense/api/AnalyticsRule.java
@@ -1,29 +1,42 @@
 package org.typesense.api;
 
 import org.typesense.api.utils.URLEncoding;
-import org.typesense.model.AnalyticsRuleDeleteResponse;
-import org.typesense.model.AnalyticsRuleSchema;
+import org.typesense.model.AnalyticsRuleUpdate;
 
 public class AnalyticsRule {
     private final ApiCall apiCall;
     private final String ruleId;
+    private final AnalyticsRuleSerializer serializer;
 
     public AnalyticsRule(String ruleId, ApiCall apiCall) {
-        this.apiCall  = apiCall;
+        this.apiCall = apiCall;
         this.ruleId = ruleId;
+        this.serializer = new AnalyticsRuleSerializer();
+    }
+    
+    public AnalyticsRule(String ruleId, ApiCall apiCall, AnalyticsRuleSerializer serializer) {
+        this.apiCall = apiCall;
+        this.ruleId = ruleId;
+        this.serializer = serializer;
     }
 
-
-    public AnalyticsRuleSchema retrieve() throws Exception {
-        return this.apiCall.get(this.getEndpoint(), null, AnalyticsRuleSchema.class);
+    public org.typesense.model.AnalyticsRule retrieve() throws Exception {
+        String response = this.apiCall.get(this.getEndpoint(), null, String.class);
+        return serializer.parseFromJson(response);
     }
 
-    public AnalyticsRuleDeleteResponse delete() throws Exception {
-        return this.apiCall.delete(this.getEndpoint(), null, AnalyticsRuleDeleteResponse.class);
+    public org.typesense.model.AnalyticsRule delete() throws Exception {
+        String response = this.apiCall.delete(this.getEndpoint(), null, String.class);
+        org.typesense.model.AnalyticsRule result = new org.typesense.model.AnalyticsRule();
+        result.name(this.ruleId);
+        return result;
+    }
+
+    public org.typesense.model.AnalyticsRule update(AnalyticsRuleUpdate rule) throws Exception {
+        return this.apiCall.put(this.getEndpoint(), rule, null, org.typesense.model.AnalyticsRule.class);
     }
 
     private String getEndpoint() {
         return AnalyticsRules.RESOURCE_PATH + "/" + URLEncoding.encodeURIComponent(ruleId);
     }
-    
 }
--- src/main/java/org/typesense/api/AnalyticsRules.java
+++ src/main/java/org/typesense/api/AnalyticsRules.java
@@ -1,30 +1,83 @@
 package org.typesense.api;
 
-import org.typesense.api.utils.URLEncoding;
-import org.typesense.model.AnalyticsRuleSchema;
-import org.typesense.model.AnalyticsRuleUpsertSchema;
-import org.typesense.model.AnalyticsRulesRetrieveSchema;
+import org.typesense.model.AnalyticsRule;
+import org.typesense.model.AnalyticsRuleCreate;
+import java.util.List;
 
 public class AnalyticsRules {
 
     private final ApiCall apiCall;
+    private final AnalyticsRuleSerializer serializer;
     public final static String RESOURCE_PATH = "/analytics/rules";
 
     public AnalyticsRules(ApiCall apiCall) {
         this.apiCall = apiCall;
+        this.serializer = new AnalyticsRuleSerializer();
+    }
+    
+    public AnalyticsRules(ApiCall apiCall, AnalyticsRuleSerializer serializer) {
+        this.apiCall = apiCall;
+        this.serializer = serializer;
     }
 
-    public AnalyticsRuleSchema create(AnalyticsRuleSchema rule) throws Exception {
-        return this.apiCall.post(RESOURCE_PATH, rule, null, AnalyticsRuleSchema.class);
+    public AnalyticsRulesResponse create(List<AnalyticsRuleCreate> rules) throws Exception {
+        String response = this.apiCall.post(RESOURCE_PATH, rules, null, String.class);
+        return parseCreateResponse(response);
     }
 
-    public AnalyticsRuleSchema upsert(String name, AnalyticsRuleUpsertSchema rule) throws Exception {
-        return this.apiCall.put(RESOURCE_PATH + "/" + URLEncoding.encodeURIComponent(name), rule, null,
-                AnalyticsRuleSchema.class);
+    public List<AnalyticsRule> retrieve() throws Exception {
+        String response = this.apiCall.get(RESOURCE_PATH, null, String.class);
+        return parseRetrieveResponse(response);
     }
 
-    public AnalyticsRulesRetrieveSchema retrieve() throws Exception {
-        return this.apiCall.get(RESOURCE_PATH, null, AnalyticsRulesRetrieveSchema.class);
+    /**
+     * Parse the create response which can be either a single AnalyticsRule or an array
+     */
+    private AnalyticsRulesResponse parseCreateResponse(String jsonResponse) throws Exception {
+        List<AnalyticsRule> rules = serializer.parseListFromJson(jsonResponse);
+        
+        for (AnalyticsRule rule : rules) {
+            if (rule.getName() == null) {
+                throw new RuntimeException("Analytics rule creation failed: rule name is null");
+            }
+        }
+        
+        return new AnalyticsRulesResponse(rules);
+    }
+    
+    /**
+     * Parse the retrieve response which is always an array of AnalyticsRule objects
+     */
+    private List<AnalyticsRule> parseRetrieveResponse(String jsonResponse) throws Exception {
+        List<AnalyticsRule> rules = serializer.parseListFromJson(jsonResponse);
+        
+        return rules;
     }
 
+    /**
+     * Response wrapper for analytics rules operations
+     */
+    public static class AnalyticsRulesResponse {
+        private final List<AnalyticsRule> rules;
+
+        public AnalyticsRulesResponse(List<AnalyticsRule> rules) {
+            this.rules = rules;
+        }
+
+        public List<AnalyticsRule> getRules() {
+            return rules;
+        }
+
+        public AnalyticsRule getFirstRule() {
+            return rules.isEmpty() ? null : rules.get(0);
+        }
+
+        public int getCount() {
+            return rules.size();
+        }
+
+        public boolean isEmpty() {
+            return rules.isEmpty();
+        }
+    }
 }
